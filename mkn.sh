a9675cc713429112f35880374a7736f1178682c1
philix <philip.deegan@gmail.com>
2016-01-03 17:58:31 +0000

    check for compilers

--- mkn.sh
+++ mkn.sh
@@ -1,6 +1,17 @@
 
 
+CXX=
+if [ -z `which $CXX` ]; then
+	CXX=clang++
+fi
+if [ -z `which clang++` ]; then
+	CXX=g++
+fi
+if [ -z `which g++` ]; then
+	echo NO COMPILER FOUND g++/clang++. ERROR EXIT
+	exit 3
+fi
 ./configure --prefix=$PWD
-make
+make CXX=${CXX}
 make install
 git update-index --assume-unchanged configure
